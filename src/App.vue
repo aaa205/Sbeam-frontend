cd0db865ded8af5900da85271c6e6a6c0c11066c
lojoyo <lojoyo@yeah.net>
2019-11-30 18:06:45 +0800

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
     <div id="app">
         <nav-bar></nav-bar>
         <router-view></router-view>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
     import NavBar from "@/components/NavBar";
+    import Footer from "./components/Footer";
     export default {
         name: 'app',
-        components: {NavBar}
+        components: {Footer, NavBar}
     }
 </script>
 
